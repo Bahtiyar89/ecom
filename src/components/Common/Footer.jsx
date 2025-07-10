ae983d911686608806893986e14bce254a2d9a0b
azic <azamathojanov96@gmail.com>
2025-07-10 18:58:42 +0300

    footer added

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { IoLogoInstagram } from 'react-icons/io';
+import { RiTwitterXLine } from 'react-icons/ri';
+import { FiPhoneCall } from 'react-icons/fi';
+import { TbBrandMeta } from 'react-icons/tb';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
@@ -37,12 +41,102 @@ const Footer = () => {
           <h3 className='text-lg text-gray-800 mb-4'>Shop</h3>
           <ul className='space-y-2 text-gray-600'>
             <li>
-              <Link className='hover:text-gray-600' to={'#'}>
-                Mens to Wear
+              <Link className='hover:text-gray-500 transition-colors' to={'#'}>
+                Mens Top Wear
+              </Link>
+            </li>
+
+            <li>
+              <Link className='hover:text-gray-500 transition-colors' to={'#'}>
+                Womens Top Wear
+              </Link>
+            </li>
+
+            <li>
+              <Link className='hover:text-gray-500 transition-colors' to={'#'}>
+                Mens Bottom Wear
+              </Link>
+            </li>
+
+            <li>
+              <Link className='hover:text-gray-500 transition-colors' to={'#'}>
+                Womens Bottom Wear
               </Link>
             </li>
           </ul>
         </div>
+
+        {/**Support links */}
+        <div>
+          <h3 className='text-lg text-gray-800 mb-4'>Support</h3>
+          <ul className='space-y-2 text-gray-600'>
+            <li>
+              <Link className='hover:text-gray-500 transition-colors' to={'#'}>
+                Contact Us
+              </Link>
+            </li>
+
+            <li>
+              <Link className='hover:text-gray-500 transition-colors' to={'#'}>
+                About Us
+              </Link>
+            </li>
+
+            <li>
+              <Link className='hover:text-gray-500 transition-colors' to={'#'}>
+                FAQs
+              </Link>
+            </li>
+
+            <li>
+              <Link className='hover:text-gray-500 transition-colors' to={'#'}>
+                Features
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/*** Follow us */}
+        <div>
+          <h3 className='text-lg text-gray-800 mb-4'>Follow Us</h3>
+          <div className='flex items-center space-x-4 mb-6'>
+            <a
+              href='https://www.facebook.com'
+              target='_blank'
+              rel='noopener'
+              className='hover:text-gray-500'
+            >
+              <TbBrandMeta className='h-5 w-5' />
+            </a>
+            <a
+              href='https://www.facebook.com'
+              target='_blank'
+              rel='noopener'
+              className='hover:text-gray-500'
+            >
+              <IoLogoInstagram className='h-5 w-5' />
+            </a>
+            <a
+              href='https://www.facebook.com'
+              target='_blank'
+              rel='noopener'
+              className='hover:text-gray-500'
+            >
+              <RiTwitterXLine className='h-4 w-4' />
+            </a>
+          </div>
+          <p className='text-gray-500'>Call Us</p>
+          <p>
+            <FiPhoneCall className='inline-block mr-2' />
+            01234-4456-789
+          </p>
+        </div>
+      </div>
+      {/**Footer Bottom */}
+      <div className='container mx-auto mt-12 px-4 lg:px-0 border-t border-gray-200 pt-6'>
+        <p className='text-gray-500 text-sm tracking-tighter text-center'>
+          2025, CompileTab. All Right Reserved.
+        </p>
       </div>
     </footer>
   );
--- src/components/Layout/UserLayout.jsx
+++ src/components/Layout/UserLayout.jsx
@@ -8,6 +8,9 @@ const UserLayout = () => {
       {/**** Header */}
       <Header />
       {/**** Main Content */}
+      <main>
+        <p>Lsasasa </p>
+      </main>
       {/**** Footer */}
       <Footer />
     </>
