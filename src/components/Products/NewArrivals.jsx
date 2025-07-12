d4eaf4e4672b4f6ec12e6197f2fdf6a0ec68afa1
bahtiyar <bahtiyar1989@gmail.com>
2025-07-12 05:57:54 +0300

    new arr

--- src/components/Products/NewArrivals.jsx
+++ src/components/Products/NewArrivals.jsx
@@ -100,6 +100,24 @@ const NewArrivals = () => {
         }
     ];
 
+    const handleMouseDown = (e)=>{
+        setIsDragging(true);
+        setStartX(e.pageX-scrollRef.current.offsetLeft);
+        setScrollLeft(scrollRef.current.scrollLeft)
+    }
+
+    const handleMouseMove=(e)=>{
+        if (!isDragging) return;
+        const x = e.pageX - scrollRef.current.offsetLeft;
+        const walk= x - startX;
+        scrollRef.current.scrollLeft=scrollLeft-walk;
+        
+    }
+
+    const handleMouseUpOrLeave=()=>{
+        setIsDragging(false);
+    }
+
     const scroll = (direction)=>{
         const scrollAmount = direction === "left"?-300:300;
         scrollRef.current.scrollBy({left:scrollAmount, behavior:"smooth"});
@@ -120,6 +138,7 @@ const NewArrivals = () => {
             scrollLeft:container.scrollLeft,
             clientWidth:container.clientWidth,
             containerScrollWidth:container.scrollWidth,
+            offsetLeft: scrollRef.current.offsetLeft
         });
         
     }
@@ -128,11 +147,12 @@ const NewArrivals = () => {
         if (container) {
            container.addEventListener("scroll", updateScrollButtons);
            updateScrollButtons(); 
+           return ()=> container.removeEventListener("scroll",updateScrollButtons);
         }
-    })
+    },[])
 
     return (
-        <section>
+        <section className='py-16 px-4 lg:px-0'>
             <div className='container mx-auto text-center mb-10 relative'>
                 <h2 className='text-3xl font-bold mb-4'>Explore New Arrivals</h2>
                 <p className='text-lg text-gray-600 mb-8'>
@@ -163,7 +183,12 @@ const NewArrivals = () => {
             {/**Scrollable Content */}
             <div 
                 ref={scrollRef} 
-                className='container mx-auto overflow-x-scroll flex space-x-6 relative'
+                className={`container mx-auto overflow-x-scroll flex space-x-6 relative 
+                    ${isDragging?"cursor-grabbing":"cursor-grab"}`}
+                onMouseDown={handleMouseDown}
+                onMouseMove={handleMouseMove}
+                onMouseUp={handleMouseUpOrLeave}
+                onMouseLeave={handleMouseUpOrLeave}
             >
                 {newArrivals.map((product)=>(
                     <div 
@@ -174,6 +199,7 @@ const NewArrivals = () => {
                             src={product.images[0]?.url} 
                             alt={product.images[0]?.altText||product.name} 
                             className='w-full h-[500px] object-cover rounded-lg'
+                            draggable='false'
                         />
                         <div className='absolute bottom-0 left-0 right-0 bg-opacity-50 backdrop-blur-md text-white p-4 rounded-b-lg '>
                             <Link to={`/product/${product._id}`} className="block">
